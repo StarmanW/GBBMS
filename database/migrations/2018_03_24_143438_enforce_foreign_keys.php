2c265cf6af71c296d1fbfef528f0cbc7b12c449f
StarmanW <swkf3011@hotmail.com>
2018-03-24 22:43:52 +0800

    Added enforce foreign key migration

---
+++ database/migrations/2018_03_24_143438_enforce_foreign_keys.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EnforceForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Add foreign keys
+        Schema::table('event_schedules', function ($table) {
+            $table->foreign('staffID')->references('staffID')->on('staff');
+            $table->foreign('eventID')->references('eventID')->on('events');
+        });
+
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->foreign('donorID')->references('donorID')->on('donors');
+            $table->foreign('eventID')->references('eventID')->on('events');
+        });
+
+        Schema::table('blood', function ($table) {
+            $table->foreign('donorID')->references('donorID')->on('donors');
+            $table->foreign('eventID')->references('eventID')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Remove all the foreign keys
+        Schema::table('event_schedules', function ($table) {
+            $table->dropForeign('staffID');
+            $table->dropForeign('eventID');
+        });
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropForeign('donorID');
+            $table->dropForeign('eventID');
+        });
+        Schema::table('blood', function ($table) {
+            $table->dropForeign('donorID');
+            $table->dropForeign('eventID');
+        });
+    }
+}
