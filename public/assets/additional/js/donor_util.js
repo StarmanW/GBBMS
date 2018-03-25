9d7e50f959eda125421722dca5d4e606334b0107
RemotePotato <RemotePotato@users.noreply.github.com>
2018-03-25 21:26:21 +0800

    Added all html files and updated asset folder

--- public/assets/additional/js/donor_util.js
+++ public/assets/additional/js/donor_util.js
@@ -14,21 +14,3 @@ function deactivateDonorAccPrompt(member_id) {
         }
     }).setHeader("Deactivate Donor Account Confirmation").show();
 }
-
-//Function to display error message when using donor registration form
-function donorRegisterError(title, errMsg) {
-    alertify.alert().setting({
-        'transition': 'zoom',
-        'movable': false,
-        'modal': true,
-        'labels': 'OK'
-    }).setHeader(title).show();
-
-    //Parse the json data
-    var msg = JSON.parse(JSON.stringify(errMsg));
-
-    //Loop through the data and display the message
-    Object.keys(msg).forEach(function (key) {
-        $('.ajs-content').append("- " + msg[key][0] + "<br/>");
-    });
-}
