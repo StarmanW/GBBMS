7b292bd1f4525432fd12bfede3e61e0ff284ad64
RemotePotato <RemotePotato@users.noreply.github.com>
2018-03-30 18:13:33 +0800

    Added new index func, old index func changed to indesShort

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -24,10 +24,26 @@ class EventController extends Controller
      */
     public function index()
     {
+        //get all upcoming events from events table for upcoming event ist page
+
+        //get events after current date and sort by date in ascending order
+        $events = Event::where('eventStatus', '=', '1')->whereDate('eventDate', '>' , DB::raw('CURDATE()'))->orderBy('eventDate', 'asc')->get();
+
+        //get nearest 3 events to current date
+        return view('donor.upcoming-event-list')->with('eventList', $events);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function indexShort()
+    {
         //get 3 upcoming events from events table for homepage
 
         //get events after current date and sort by date in ascending order
-        $events = Event::where('eventStatus', '=', 'true')->whereDate('eventDate', '>' , DB::raw('CURDATE()'))->orderBy('eventDate', 'asc')->get();
+        $events = Event::where('eventStatus', '=', '1')->whereDate('eventDate', '>' , DB::raw('CURDATE()'))->orderBy('eventDate', 'asc')->get();
 
         //get nearest 3 events to current date
         $eventList = array($events[0], $events[1], $events[2]);
@@ -64,7 +80,7 @@ class EventController extends Controller
     public function show($id)
     {
         //get one event for detail page
-        $event = Event::where("eventID", $id)->get();
+        $event = Event::where("eventID", $id)->get(); //change to find?
         return view('donor.event-details-donor')->with('event', $event);
     }
 
