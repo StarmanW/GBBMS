1624162bef9782443684d57117d89d3ade2c1878
RemotePotato <RemotePotato@users.noreply.github.com>
2018-04-04 02:50:33 +0800

    Added index func for nurse

--- app/Http/Controllers/StaffEventController.php
+++ app/Http/Controllers/StaffEventController.php
@@ -47,7 +47,23 @@ class StaffEventController extends Controller {
 
         //get nearest 3 events to current date
         $eventList = array($events[0], $events[1], $events[2]);
-        return view('staff.home-hr')->with('eventList', $eventList);
+        return view('staff.homepage-hr')->with('eventList', $eventList);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function indexShortNurse() {
+        //get 3 upcoming events from events table for homepage
+
+        //get events after current date and sort by date in ascending order
+        $events = Event::where('eventStatus', '=', '1')->whereDate('eventDate', '>', DB::raw('CURDATE()'))->orderBy('eventDate', 'asc')->get();
+
+        //get nearest 3 events to current date
+        $eventList = array($events[0], $events[1], $events[2]);
+        return view('staff.homepage-nurse')->with('eventList', $eventList);
     }
 
     /**
