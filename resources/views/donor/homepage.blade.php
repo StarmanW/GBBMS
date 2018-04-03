e9e3eb70eae2e4efbc21999c46846470f21936e2
RemotePotato <RemotePotato@users.noreply.github.com>
2018-04-04 02:50:18 +0800

    Removed duplicate code

--- resources/views/donor/homepage.blade.php
+++ resources/views/donor/homepage.blade.php
@@ -51,7 +51,6 @@
             <h3 class="mbr-section-subtitle pb-5 mbr-fonts-style display-5"></h3>
 
             <div class="container timelines-container" mbri-timelines="">
-
                 <?php $order = "" ?>
                 @foreach ($eventList as $event)
                     <?php $order = ($order == "reverse") ? '' : 'reverse' ?>
@@ -105,131 +104,6 @@
                         </div>
                     </div>
                 @endforeach
-                {{--<div class="row timeline-element reverse separline">--}}
-                    {{--<div class="timeline-date-panel col-xs-12 col-md-6  align-left">--}}
-                        {{--<div class="time-line-date-content">--}}
-                            {{--<p class="mbr-timeline-date mbr-fonts-style display-5">2 May 2018</p>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                    {{--<span class="iconBackground"></span>--}}
-                    {{--<div class="col-xs-12 col-md-6 align-right">--}}
-                        {{--<div class="timeline-text-content">--}}
-                            {{--<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">Kota Kinabalu Charity Blood Drive May 2018</h4>--}}
-                            {{--<table class="activity-table">--}}
-                                {{--<tbody>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Room&nbsp;&nbsp;:&nbsp;&nbsp;</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">031012, Quadrant 1, 3rd floor, Gleneagles Hospital KK</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Date&emsp;:</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">12 May 2018</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Time&emsp;:</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">10.30 AM to 4.00 PM</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--</tbody>--}}
-                            {{--</table>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-
-                {{--<div class="row timeline-element  separline">--}}
-                    {{--<div class="timeline-date-panel col-xs-12 col-md-6 align-right">--}}
-                        {{--<div class="time-line-date-content">--}}
-                            {{--<p class="mbr-timeline-date mbr-fonts-style display-5">1 June 2018</p>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                    {{--<span class="iconBackground"></span>--}}
-                    {{--<div class="col-xs-12 col-md-6 align-left ">--}}
-                        {{--<div class="timeline-text-content">--}}
-                            {{--<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">Blood Week June 2018</h4>--}}
-                            {{--<table class="activity-table">--}}
-                                {{--<tbody>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Room&nbsp;&nbsp;:&nbsp;&nbsp;</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">031012, Quadrant 1, 3rd floor, Gleneagles Hospital KK</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Date&emsp;:</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">12 May 2018</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Time&emsp;:</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">10.30 AM to 4.00 PM</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--</tbody>--}}
-                            {{--</table>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-
-                {{--<div class="row timeline-element reverse">--}}
-                    {{--<div class="timeline-date-panel col-xs-12 col-md-6  align-left">--}}
-                        {{--<div class="time-line-date-content">--}}
-                            {{--<p class="mbr-timeline-date mbr-fonts-style display-5">12 October 2018</p>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                    {{--<span class="iconBackground"></span>--}}
-                    {{--<div class="col-xs-12 col-md-6 align-right">--}}
-                        {{--<div class="timeline-text-content">--}}
-                            {{--<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">Kota Kinabalu Charity Blood Drive October 2018</h4>--}}
-                            {{--<table class="activity-table">--}}
-                                {{--<tbody>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Room&nbsp;&nbsp;:&nbsp;&nbsp;</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">031012, Quadrant 1, 3rd floor, Gleneagles Hospital KK</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Date&emsp;:</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">12 May 2018</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">Time&emsp;:</p>--}}
-                                    {{--</td>--}}
-                                    {{--<td>--}}
-                                        {{--<p class="mbr-timeline-text mbr-fonts-style display-7">10.30 AM to 4.00 PM</p>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--</tbody>--}}
-                            {{--</table>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
             </div>
         </div>
     </section>
