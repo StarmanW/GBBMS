ad1fa1e496d703c8ea7bffe878927b49ab0b7e2b
RemotePotato <RemotePotato@users.noreply.github.com>
2018-04-13 23:32:55 +0800

    Updated side bar + small changes

--- resources/views/staff/staff-list.blade.php
+++ resources/views/staff/staff-list.blade.php
@@ -5,24 +5,23 @@
 @endsection
 
 @section('additionalCSS')
-    <link rel="stylesheet" href="/assets/datatables/data-tables.bootstrap4.min.css">
+    <link rel="stylesheet" href={{"/assets/datatables/data-tables.bootstrap4.min.css"}}>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="/assets/additional/css/table-list.css" type="text/css">
-    <link rel="stylesheet" href="/assets/additional/css/sidebar.css" type="text/css">
+    <link rel="stylesheet" href={{"/assets/additional/css/table-list.css"}} type="text/css">
+    <link rel="stylesheet" href={{"/assets/additional/css/sidebar.css"}} type="text/css">
 @endsection
 
 @section('contents')
     <!-- Sidebar -->
     <div class="w3-sidebar w3-bar-block w3-animate-left" style="display:none;z-index:5" id="mySidebar">
-        <a href="/staff/hr/home" class="w3-bar-item" id="homepageStyle">
-            <img src="assets/images/gLogo.png" alt="Gleneagles Logo" title="" style="height: 5rem;"> Gleneagles KK
-        </a>
+        <br><br><br><br>
+        <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+        <br>
         <span style="text-align: center; color: white" class="w3-bar-item">View List</span>
         <hr id="sideBarHR">
         <a href="/staff/hr/list/donor" class="w3-bar-item w3-button">Donor List</a>
         <a href="/staff/hr/list/staff" class="w3-bar-item w3-button">Staff List</a>
         <a href="/staff/hr/list/event" class="w3-bar-item w3-button">Event List</a>
-        <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
     </div>
 
     <!-- Page Content -->
@@ -35,10 +34,10 @@
                 <i class="fa  fa-angle-up fa-3x" id="sidebar-toggle" aria-hidden="true"></i>
             </a>
             <div class="container container-table">
+                <h2 class="mbr-section-title mbr-fonts-style align-center pb-3 display-2">Staff List</h2>
                 <h3 class="mbr-section-subtitle mbr-fonts-style align-center pb-5 mbr-light display-5"></h3>
                 <div class="table-backpanel">
                     <div class="table-wrapper">
-                        <h2 class="mbr-section-title mbr-fonts-style align-center pb-3 display-2">Staff List</h2>
                         <div class="container">
                             <div class="row search">
                                 <div class="col-md-6"></div>
