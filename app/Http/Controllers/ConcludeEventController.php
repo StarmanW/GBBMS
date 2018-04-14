749ba0f03fa259f2ae8ceba88d0e9c0cfe86e0e5
StarmanW <swkf3011@hotmail.com>
2018-04-14 19:29:13 +0800

    Fix set event schedule status in conclude event

--- app/Http/Controllers/ConcludeEventController.php
+++ app/Http/Controllers/ConcludeEventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use App\EventSchedule;
 use App\Reservation;
 use Illuminate\Http\Request;
 
@@ -24,29 +25,35 @@ class ConcludeEventController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function update($id) {
-        //set current event status to completed
+        //Set current event status to completed
         //and donor reservation status to 'did not attend' for donors that did not attend
 
-        //set event status
+        //Set event status
         $event = Event::find($id);
         $event->eventStatus = 0;
 
-        //set donor reservation status
+        //Set donor reservation status
         $reservations = Reservation::where('eventID', '=', $event->eventID)->where('resvStatus', '=', 1)->get();
 
         $resvSaveStats = 0;
         foreach ($reservations as $reservation) {
             $reservation->resvStatus = 2;
 
-            if ($reservation->save())
+            if ($reservation->save() === true)
                 $resvSaveStats++;
         }
 
-        //set staff schedule status
-        $schedule = find($id);
-        $schedule->schedStatus = 0;
+        //Set event schedules status
+        $schedules = EventSchedule::where('schedStatus', '=', 1)->where('eventID', '=', $id)->get();
+        $schedSaveStats = 0;
+        foreach ($schedules as $schedule) {
+            $schedule->schedStatus = 0;
 
-        if ($event->save() && $resvSaveStats === count($reservations))
+            if ($schedule->save() === true)
+                $schedSaveStats++;
+        }
+
+        if ($event->save() && $resvSaveStats === count($reservations) && $schedSaveStats === count($schedules))
             return redirect('/staff/nurse/homepage')->with('success', 'Event has been successfully concluded!');
         else
             return redirect()->back()->with('failure', 'Event was not concluded, please try again.');
