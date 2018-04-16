21b4e6599f9980fc4f2750c41b5ce7b909272c29
StarmanW <swkf3011@hotmail.com>
2018-04-16 19:32:45 +0800

    Minor PDF config change

--- config/pdf.php
+++ config/pdf.php
@@ -8,5 +8,5 @@ return [
 	'keywords'              => '',
 	'creator'               => '',
 	'display_mode'          => 'fullpage',
-	'tempDir'               => base_path('../temp/')
+	'tempDir'               => base_path('/storage/app/public/temp/')
 ];
