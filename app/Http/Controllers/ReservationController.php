8ee872161e5f4e79dedb1f1a92afbf0ff82f917a
RemotePotato <RemotePotato@users.noreply.github.com>
2018-04-18 22:31:59 +0800

    Chnaged comments, rearranged some code, removed unsued code

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -26,12 +26,13 @@ class ReservationController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        //get all reservations for reservation history list page
-        //find id of current user
+        //get current user donor
         $donor = Donor::find(Auth::user()->donorID);
 
+        //get reservations history for the user and paginate into set of 10
         $resvs = Reservation::where('donorID', '=', $donor->donorID)->where('resvStatus', '!=', 1)->paginate(10);
 
+        //return result to reservation history list page
         return view('donor.resv-list')->with('resvs', $resvs);
     }
 
@@ -41,43 +42,14 @@ class ReservationController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function resvCurrent() {
-        //get all reservations for reservation history list page
-        //find id of current user
+        //get current user donor
         $donor = Donor::find(Auth::user()->donorID);
 
+        //get current reservations for the user and paginate into set of 10
         $resvs = Reservation::where('donorID', '=', $donor->donorID)->where('resvStatus', '=', 1)->paginate(10);
 
+        //return result to reservation list page
         return view('donor.resv-current')->with('resvs', $resvs);
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function indexShort() {
-        //get all reservations for reservation history list page
-        //find id of current user
-        $donor = Donor::find(Auth::user()->donorID);
-
-        $resvs = DB::table('reservations')
-            ->select();
-
-        $resv = array();
-
-        for ($i = 0; $i < count($resvs); $i++) {
-            if ($resvs[$i]->events->eventDate > Carbon::now()) {
-                $resv[$i] = $resvs[$i];
-            }
-        }
-
-        $unsortedResv = array_sort($resv);
-
-        $resvList = [$unsortedResv[0], $unsortedResv[1], $unsortedResv[2]];
-
-        dd($resvList);
-
-        return view('donor.resv-list')->with('resvList', $resvList);
     }
 
     /**
@@ -110,6 +82,7 @@ class ReservationController extends Controller {
         //get current date
         $currentDate = Carbon::now();
 
+        //Check for recent donation
         foreach ($lastDonation as $donation) {
             //get event date
             $donationEventDate = Carbon::createFromTimestamp(strtotime($donation->events->eventDate));
@@ -117,11 +90,19 @@ class ReservationController extends Controller {
             //get date 3 months from last donation
             $threeMntsFrmDate = $donationEventDate->addMonths(3);
 
+            //check if last donation date is 3 months ago
             if ($currentDate <= $threeMntsFrmDate) {
                 //get date difference
                 $dateDiff = $threeMntsFrmDate->diff($currentDate)->days + 1;
 
-                return redirect('/donor/upcoming-events')->with('failure', 'You have donated blood at ' . date_format(date_create($donation->events->eventDate), 'd F Y') . ' Please try again after ' . $dateDiff . ' days. (After ' . date_format($donationEventDate, 'd F Y') . ')');
+                //return to upcoming events page with message
+                return redirect('/donor/upcoming-events')->with
+                    (
+                        'failure', 'You have donated blood at ' .
+                        date_format(date_create($donation->events->eventDate), 'd F Y') .
+                        ' Please try again after ' . $dateDiff . ' days. 
+                        (After ' . date_format($donationEventDate, 'd F Y') . ')'
+                    );
             }
         }
 
@@ -134,10 +115,18 @@ class ReservationController extends Controller {
             $donationEventDate = Carbon::createFromTimestamp(strtotime($donation->events->eventDate));
             //get date 3 months from last donation
             $threeMntsFrmDate = $donationEventDate->addMonths(3);
+
+            //check if last reservation date is 3 months ago
             if ($currentDate <= $threeMntsFrmDate) {
                 //get date difference
                 $dateDiff = $threeMntsFrmDate->diff($currentDate)->days + 1;
-                return redirect('/donor/upcoming-events')->with('failure', 'You have recently reserved a blood donation event that is within the 3 months period. Please try again after ' . $dateDiff . ' days. (After ' . date_format($donationEventDate, 'd F Y') . ')');
+
+                //return to upcoming events page with message
+                return redirect('/donor/upcoming-events')->with
+                    (
+                        'failure', 'You have recently reserved a blood donation event that is within the 3 months period. 
+                        Please try again after ' . $dateDiff . ' days. (After ' . date_format($donationEventDate, 'd F Y') . ')'
+                    );
             }
         }
 
@@ -148,7 +137,7 @@ class ReservationController extends Controller {
         if ($prevCancelledResv !== null) {
             $prevCancelledResv->resvStatus = 1;     //Update 'cancelled' status back to 'reserved'
             
-            //Redirect to upcoming-events page with appropriate message
+            //return to upcoming events page with message
             if ($prevCancelledResv->save())
                 return redirect('/donor/upcoming-events/' . $prevCancelledResv->eventID)->with('success', 'Reservation has been successfully made!');
             else
@@ -166,6 +155,7 @@ class ReservationController extends Controller {
         $resv->resvDateTime = Carbon::now();
         $resv->resvStatus = 1;
 
+        //if save successful, return to upcoming events page with message
         if ($resv->save())
             return redirect('/donor/upcoming-events/' . $resv->eventID)->with('success', 'Reservation has been successfully made!');
         else
@@ -187,22 +177,27 @@ class ReservationController extends Controller {
             session(['isResvHistory' => true]);
         }
 
+        //find a specific reservation
         $reservation = Reservation::find($id);
+
+        //return result to reservation detail page
         return view('donor.resv-details')->with('reservation', $reservation);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Deactivate a reservation record without deleting.
      *
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function deactivate($id) {
-        //deactivate a reservation recor without deleting
+        //find a specific reservation
         $resv = Reservation::find($id);
+
+        //set reservation status to "Donor cancelled"
         $resv->resvStatus = 3;
 
-        //make new current reservation page first
+        //return to current page with message
         if ($resv->save())
             return redirect()->back()->with('success', 'Reservation has been successfully cancelled!');
         else
